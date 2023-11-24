af3c0412ea6520fb721e4db0832ba4713f9926dc
ThomasGEMA <thomas.goutieres.edu@groupe-gema.com>
2023-11-24 14:52:02 +0100

    Update Bataille.py

--- Bataille.py
+++ Bataille.py
@@ -19,47 +19,51 @@ class Bataille:
         ]
 
     def afficher_grille(self):
-        #grille = [[0 for _ in range(10)] for _ in range(10)]
-
-        # Créer des étiquettes pour les colonnes et les lignes
+   # Créer des étiquettes pour les colonnes et les lignes
         colonnes = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10']
         lignes = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J']
 
-        # Afficher les étiquettes des colonnes
+   # Afficher les étiquettes des colonnes
         print(' ', end='')
         for c in colonnes:
             print(' ' + str(c), end='')
         print()
 
-        # Afficher la grille avec les étiquettes des lignes
+   # Afficher la grille avec les étiquettes des lignes
         for i,e in enumerate(self.grille1) : # toutes les lignes
             print(lignes[i], end='')
-            for elt in e:   # toutes les cases de la ligne courange
-                print(' ' + str(elt), end='')
+            for elt in e:  # toutes les cases de la ligne courante
+                if elt == 1:
+                    print(' ' + 'X', end='')
+                else:
+                    print(' ' + str(elt), end='')
             print()
-
 
     def placer_bateaux(self):
         for bateau in self.bateaux1:
             while True:
-                x = int(input(f"Entrez la coordonnée X du {bateau.get_taille()}-bateau : "))
-                y = int(input(f"Entrez la coordonnée Y du {bateau.get_taille()}-bateau : "))
-                horizontal = input("Le bateau est-il horizontal ? (o/n) ").lower() == 'o'
+                try:
+                    x = int(input(f"Entrez la coordonnée X du {bateau.get_taille()}-bateau : "))
+                    y = int(input(f"Entrez la coordonnée Y du {bateau.get_taille()}-bateau : "))
+                    horizontal = input("Le bateau est-il horizontal ? (o/n) ").lower() == 'o'
 
-                bateau.avancee(x - bateau.get_x(), y - bateau.get_y())
-                bateau.horizontal = horizontal
+                    bateau.avancee(x - bateau.get_x(), y - bateau.get_y())
+                    bateau.horizontal = horizontal
 
-                if self.peut_placer_bateau(bateau):
-                    self.placer_bateau_sur_grille(bateau)
-                    break
-                else:
-                    print("Impossible de placer le bateau à cet endroit. Réessayez.")
+                    if self.peut_placer_bateau(bateau):
+                        self.placer_bateau_sur_grille(bateau)
+                        break
+                    else:
+                        print("Impossible de placer le bateau à cet endroit. Réessayez.")
+                except ValueError:
+                    print("Veuillez entrer un nombre valide pour la coordonnée X et Y.")
+
     def peut_placer_bateau(self, bateau):
         if bateau.horizontal:
             return (
                 bateau.get_x() + bateau.get_taille() <= 10
                 and all(
-                    self.grille[bateau.get_y() - 1][x - 1] == 0
+                    self.grille1[bateau.get_y() - 1][x - 1] == 0
                     for x in range(bateau.get_x(), bateau.get_x() + bateau.get_taille())
                 )
             )
@@ -81,7 +85,8 @@ class Bataille:
                 self.grille1[y - 1][bateau.get_x() - 1] = 1
 
 if __name__ == '__main__':
-    cl = Bataille()
-    cl.__init__()
-    cl.afficher_grille()
-    cl.placer_bateaux()
+   cl = Bataille()
+   cl.__init__()
+   cl.afficher_grille()
+   cl.placer_bateaux()
+   cl.afficher_grille()
